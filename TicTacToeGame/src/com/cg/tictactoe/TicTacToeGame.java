283927b6ca4a756b435a2453ba12a82ff192c92d
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-10-01 13:06:28 +0530

    Added a method for selecting the location on the move

--- TicTacToeGame/src/com/cg/tictactoe/TicTacToeGame.java
+++ TicTacToeGame/src/com/cg/tictactoe/TicTacToeGame.java
@@ -2,6 +2,10 @@ package com.cg.tictactoe;
 
 import java.util.*;
 
+/**
+ * @author Abhishek Bhui
+ *
+ */
 public class TicTacToeGame {
 	Scanner sc = new Scanner(System.in);
 
@@ -25,18 +29,37 @@ public class TicTacToeGame {
 		char userInput = sc.next().charAt(0);
 		return userInput;
 	}
-	
-	public static void showBoard(char[] board)
-	{
-		System.out.println(board[1]+" | "+board[2]+" | "+board[3]+"\n");
-		System.out.println(board[4]+" | "+board[5]+" | "+board[6]+"\n");
-		System.out.println(board[7]+" | "+board[8]+" | "+board[9]+"\n");
+
+	/**
+	 * UC3 Show the board
+	 */
+	public static void showBoard(char[] board) {
+		System.out.println(board[1] + " | " + board[2] + " | " + board[3] + "\n");
+		System.out.println(board[4] + " | " + board[5] + " | " + board[6] + "\n");
+		System.out.println(board[7] + " | " + board[8] + " | " + board[9] + "\n");
+	}
+
+	/**
+	 * UC4 Select the location
+	 */
+	public static void selectLocation(char[] board, char player) {
+
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Select the location");
+			int location = sc.nextInt();
+			if (board[location] != ' ')
+				System.out.println("location occupied");
+			else
+				board[location]=player;
+				showBoard(board);
+		}
 	}
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Welcome to Tic Tac Toe game!");
-		char[] boardValue=createBoard();
+		char[] board = createBoard();
 		char comp, player;
 		player = inputLetter();
 		if (player == 'X' || player == 'x')
@@ -45,7 +68,10 @@ public class TicTacToeGame {
 			comp = 'X';
 
 		System.out.println("computer letter is: " + comp + "\nplayer letter is: " + player);
-		showBoard(boardValue);
+		selectLocation(board, player);
+		/* board[1]='O'; */
+
+		showBoard(board);
 	}
 
 }
