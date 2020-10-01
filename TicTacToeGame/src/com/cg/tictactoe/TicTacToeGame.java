a468211b4c2bbfcc90c9e64d119c7162246c6690
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-10-01 10:26:21 +0530

    Added a method to ask the user to decide a letter

--- TicTacToeGame/src/com/cg/tictactoe/TicTacToeGame.java
+++ TicTacToeGame/src/com/cg/tictactoe/TicTacToeGame.java
@@ -1,15 +1,38 @@
 package com.cg.tictactoe;
 
+import java.util.*;
+
 public class TicTacToeGame {
-	public static void createBoard() {
+	/**
+	 * UC1 Creating Board
+	 */
+	public static char[] createBoard() {
 		char[] board = new char[10];
-		for (int index = 0; index < 10; index++) {
+		for (int index = 1; index < 10; index++) {
 			board[index] = ' ';
 		}
+		return board;
+	}
+
+	public static char inputLetter() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the letter(X/O)");
+		char ch = sc.next().charAt(0);
+		return ch;
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe game!");
 		createBoard();
+		char comp, player;
+		player = inputLetter();
+		if (player == 'X') {
+			comp = 'O';
+		} else {
+			comp = 'X';
+		}
+		System.out.println("computer letter is: " + comp);
+		System.out.println("player letter is: " + player);
 	}
+
 }
